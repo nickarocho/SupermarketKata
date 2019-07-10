f3aa1ba196ff5562c15c19ad1dbb03a32767eebf
Nick Arocho <nick@boldlyfine.com>
2019-07-10 16:11:17 -0700

    Add constructor class, create basic logic

--- Checkout.js
+++ Checkout.js
@@ -1,3 +1,15 @@
 module.exports = class Checkout {
-
+    constructor() {
+        this.prices = new Object();
+        this.total = 0;
+    }
+    addItemPrice(item, price) {
+        this.prices[item] = price;
+    }
+    addItem(item) {
+        this.total += this.prices[item];
+    }
+    calculateTotal() {
+        return this.total;
+    }
 }
--- test/checkout.spec.js
+++ test/checkout.spec.js
@@ -1,6 +1,21 @@
-const Checkout = require('../checkout');
+const Checkout = require('../Checkout');
 var expect = require('chai').expect;
 
-it('Can instantiate checkout', function() {
+beforeEach(function() {
+    checkout = new Checkout();
+});
+
+it('Can calculate the current total', function() {
     var checkout = new Checkout();
+    checkout.addItemPrice('a', 1);
+    checkout.addItem('a');
+    expect(checkout.calculateTotal()).to.equal(1);
+});
+
+it('Can add multiple items and get current total', function () {
+    checkout.addItemPrice('a', 1);
+    checkout.addItemPrice('b', 2);
+    checkout.addItem('a');
+    checkout.addItem('b');
+    expect(checkout.calculateTotal()).to.equal(3);
 });
